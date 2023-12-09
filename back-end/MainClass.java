ca2cd291ad73cd10aad32aab7bcc90afafb3cc31
Deep Raj <121842502+CodewithRajDeep@users.noreply.github.com>
2023-12-09 20:52:13 +0530

    Update MainClass.java

--- back-end/MainClass.java
+++ back-end/MainClass.java
@@ -7,29 +7,25 @@ import java.io.IOException;
 public class MainClass {
 
     public static void main(String[] args) {
-        // Get the current working directory
+      
         String directoryPath = "F:/My work/word-generation/";
 
-        // Specify the relative path to the data file
+     
         String relativePath = "shakespeare.txt";
-        // String relativePath = "words.txt"//
-
-        // Create a File object using the current directory and relative path
+     
         File dataFile = new File(directoryPath, relativePath);
 
-        // Check if the file exists
+    
         if (dataFile.exists()) {
-            // Create an instance of StatsObj
+           
             StatsObj statsObj = new StatsObj(5);
 
-            // Read the content of the data file and extract words
+            
             try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    // Split the line into words using space as a delimiter
+                   
                     String[] words = line.split("\\s+");
-
-                    // Add each word to StatsObj
                     for (String word : words) {
                         statsObj.add(word);
                     }
@@ -38,7 +34,6 @@ public class MainClass {
                 e.printStackTrace();
             }
 
-            // Test the functionality of StatsObj
             System.out.println("Count of 'shakespeare.txt': " + statsObj.getCountOf(dataFile.getPath()));
             System.out.println("Most frequent item: " + statsObj.getMostFrequent());
             System.out.println("Top most frequent items: " + statsObj.getTopMostFreq());
