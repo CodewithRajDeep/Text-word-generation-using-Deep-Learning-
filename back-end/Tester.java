3cc625a3683b85b536430ce1acbd946dc1da7ab7
Deep Raj <121842502+CodewithRajDeep@users.noreply.github.com>
2023-12-09 20:55:15 +0530

    Update Tester.java

--- back-end/Tester.java
+++ back-end/Tester.java
@@ -3,24 +3,24 @@ public class Tester {
     private static final int NUM_WORDS_TO_GENERATE = 100;
 
     public static void main(String[] args) {
-        TextModel model = new TextModel(); // create your model
-        model.loadPairData("shakespeare.txt"); // load the data
+        TextModel model = new TextModel(); 
+        model.loadPairData("shakespeare.txt"); 
 
         String generatedOutput = "";
 
-        String currentWord = "From fairest"; // Choose starting word
+        String currentWord = "From fairest"; 
         generatedOutput += currentWord;
 
         for (int i = 0; i < NUM_WORDS_TO_GENERATE; i++) {
             String nextWord = (model).simulate2NextWord(currentWord);
             nextWord = nextWord.substring(nextWord.indexOf(" ") + 1, nextWord.length());
 
-            /* System.out.println(nextWord) */
-            // System.out.println(currentWord)
+           
+            
             generatedOutput += " " + nextWord.substring(nextWord.indexOf(" ") + 1, nextWord.length());
 
             if (i % 8 == 0)
-                generatedOutput += "\n"; // add some line breaks in the output
+                generatedOutput += "\n"; 
             currentWord = nextWord;
         }
 
